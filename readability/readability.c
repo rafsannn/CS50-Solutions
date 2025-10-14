f9a655d547b4b76500b4e1e92f582a5e36ffcb01
MD Rafsan Rahman Rumman <rafsanrahmanrumman@gmail.com>
2025-10-15 01:29:57 +0600

    still_unfinished

--- readability/readability.c
+++ readability/readability.c
@@ -1,12 +1,29 @@
-#include <cs50.h>
+#include <string.h>
 #include <stdio.h>
 
 int main(void)
 {
     //Take input of users text
-    char text[100];
-    // Figure out the number of sentences by counting number of full stops
-    // Figure out the number of words by counting the spaces + 1
+    char text[1000];
+    printf("Text: ");
+    fgets(text, sizeof(text), stdin);
+
+    // Figure out the number of sentences and words
+    int sentence_count = 0, word_count = 1;
+    for(int i = 0; text[i] != '\0'; i++)
+    {
+        if (text[i] == '.' || text[i] == '?' || text[i] == '!')
+        {
+            sentence_count++;
+        }
+        if (text[i] == ' ')
+        {
+            word_count++;
+        }
+    }
+    float sentence_rate = ((strlen(text) + 1) / word_count) * 100;
+    printf("%f\n", sentence_rate);
     // Calculate the percentage ot sentences and words
+
     // Apply 
 }
